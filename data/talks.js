d04ff32cf72028703955c004be9211176fe6a116
Kyle Coberly <kyle.coberly@gmail.com>
2018-12-28 21:34:04 -0700

    Adding logos to talks page

--- data/talks.js
+++ data/talks.js
@@ -8,6 +8,7 @@ module.exports.talks = [{
         url: "https://slides.com/kylecoberly/deck#/",
     }],
     abstract: "A presentation on how to manage state in an Ember application.",
+    iconUrl: "/assets/ember.png",
 }, {
     title: "Suck Less At Networking",
     date: "2018-10-18",
@@ -18,6 +19,7 @@ module.exports.talks = [{
         url: "https://github.com/developdenver/suck-less-at-networking",
     }],
     abstract: "A workshop from Develop Denver 2018 on improving your networking skills.",
+    iconUrl: "/assets/briefcase.png",
 }, {
     title: "Suck Less At Interviewing",
     date: "2018-10-18",
@@ -28,6 +30,7 @@ module.exports.talks = [{
         url: "https://github.com/developdenver/suck-less-at-interviewing",
     }],
     abstract: "A workshop from Develop Denver 2018 on improving your interviewing skills.",
+    iconUrl: "/assets/briefcase.png",
 }, {
     title: "Application Layout With Ember",
     date: "2016-03-16",
@@ -38,6 +41,7 @@ module.exports.talks = [{
         url: "https://slides.com/kylecoberly/deck-6-21#/",
     }],
     abstract: "A presentation on how to structure an Ember app to match your visual hierarchy.",
+    iconUrl: "/assets/ember.png",
 }, {
     title: "Customizing Ember Data",
     date: "2015-09-25",
@@ -48,6 +52,7 @@ module.exports.talks = [{
         url: "https://slides.com/kylecoberly/customizing-ember-data#/",
     }],
     abstract: "A presentation on how to override Ember Data's powerful defaults to meet your application's needs.",
+    iconUrl: "/assets/ember.png",
 },{
     title: "Inheritance & Composition With Vue Components",
     date: "2018-11-25",
@@ -58,6 +63,7 @@ module.exports.talks = [{
         url: "https://slides.com/kylecoberly/inheritance-and-composition-with-vue-components#/",
     }],
     abstract: "A lightning talk on Vue inheritance and Vue mixins.",
+    iconUrl: "/assets/vue.png",
 }, {
     title: "App Architecture with Vue",
     date: "2018-07-22",
@@ -74,6 +80,7 @@ module.exports.talks = [{
         url: "https://docs.google.com/document/d/1xRdwHND1fKuEU8HNYEdWpMN9V4EtV11oErLGgZPvJhs/edit?usp=sharing",
     }],
     abstract: "A presentation on the model-view-component architecture pattern in Vue.",
+    iconUrl: "/assets/vue.png",
 }, {
     title: "Front-End/Face-Off - Round 2",
     date: "2018-09-20",
@@ -81,6 +88,7 @@ module.exports.talks = [{
     venue: "Denver Node",
     slug: "front-end-face-off-round-2",
     abstract: "Brooks, CJ, and I returned for round 2 of the Front-End/Face-Off. This time, I represented Team Vue.",
+    iconUrl: "/assets/vue.png",
 }, {
     title: "Unlearning Business",
     date: "2018-07-17",
@@ -88,6 +96,7 @@ module.exports.talks = [{
     venue: "Denver Leadership Meetup",
     slug: "unlearning-business",
     abstract: "I spoke at the Denver Leadership meetup about how and why we should stop \"playing business\" and turn off our autopilot with some common business ideas.",
+    iconUrl: "/assets/briefcase.png",
 }, {
     title: "AMA - Full-Stack Engineer",
     date: "2018-04-10",
@@ -95,6 +104,7 @@ module.exports.talks = [{
     venue: "Video Conference",
     slug: "ama-full-stack-engineer",
     abstract: "I spoke with a [group of recruiters](https://www.eventbrite.com/e/ask-me-anything-fullstack-engineer-tickets-44247970872) about full-stack engineering is, what I think distinguishes full-stackers from specialists, and what I look for in a job opportunity.",
+    iconUrl: "/assets/web.jpg",
 },{
     title: "Double-Loop TDD",
     date: "2018-03-27",
@@ -108,13 +118,15 @@ module.exports.talks = [{
         url: "https://github.com/kylecoberly/double-loop-tdd",
     }],
     slug: "double-loop-tdd",
-    abstract: "I spoke about the practice of double-loop TDD, which combines high and low level tests to provide tight feedback loops that also keep you focused on the big picture. "
+    abstract: "I spoke about the practice of double-loop TDD, which combines high and low level tests to provide tight feedback loops that also keep you focused on the big picture.",
+    iconUrl: "/assets/agile.jpeg",
 }, {
     title: "Web Application Architecture Guest Lecture",
     date: "2017-11-16",
     venue: "Metropolitan State University of Denver",
     slug: "metro-web-application-architecture",
     abstract: "I spoke to a class of about 30 Computer Science students about the MVC architecture pattern, how that's probably not the metaphor we would use if we were starting over today, and the state of modern web development.",
+    iconUrl: "/assets/web.jpg",
 },{
     title: "Everything You've Ever Wanted To Know From a Technologist But Were Too Shy To Ask",
     date: "2017-09-25",
@@ -138,6 +150,7 @@ In addition to myself, the panel included:
 * Ashly Carter, UX Designer at Deloitte Digital
 * Kirk Moore, Data Strategist, formerly SVP of Consumer Experience at Craftsy
     `,
+    iconUrl: "/assets/web.jpg",
 }, {
     title: "The 5 Developers I Never Want To Work With Again",
     date: "2017-08-11",
@@ -153,6 +166,7 @@ In addition to myself, the panel included:
     }],
     slug: "the-5-developers-i-never-want-to-work-with-again",
     abstract: "A sardonic look at how we act as developers, how we think of developers, and why we need to change both them.",
+    iconUrl: "/assets/briefcase.png",
 }, {
     title: "Isn't Ember Dead?",
     date: "2017-07-27",
@@ -174,6 +188,7 @@ In addition to myself, the panel included:
     }],
     slug: "isnt-ember-dead",
     abstract: "The Front-End/Face-Off was put on by the Denver Node.js meetup. It was a common set of stories that were live-coded in front of an audience in Ember, Angular, React, and Vanilla. I coded the app in Ember, and give some insight into the thought process of an Ember developer.",
+    iconUrl: "/assets/ember.png",
 }, {
     title: "Ember Test Selectors and Page Objects",
     date: "2017-05-31",
@@ -184,6 +199,7 @@ In addition to myself, the panel included:
     }],
     slug: "ember-test-selectors-and-page-objects",
     abstract: "A lightning talk about how to use Ember Test Selectors and Ember Page Objects to add healthy abstractions and flexibility in your Ember tests.",
+    iconUrl: "/assets/ember.png",
 },{
     title: "Approaches To Innovation",
     date: "2017-05-25",
@@ -194,6 +210,7 @@ In addition to myself, the panel included:
     }],
     slug: "approaches-to-innovation",
     abstract: "A guest lecture on innovation for DU's masters program in Strategic Innovation and Change.",
+    iconUrl: "/assets/briefcase.png",
 },{
     title: "Progressive Web Apps Are The Future",
     date: "2017-05-17",
@@ -212,6 +229,7 @@ In addition to myself, the panel included:
     }],
     slug: "progressive-web-apps-are-the-future",
     abstract: "A talk for the TechCrunch Disrupt NYC conference where I talk about the history of the web, and how some of the new browser technologies take on the advantages of native app with few of their drawbacks.",
+    iconUrl: "/assets/pwa.jpg",
 }, {
     title: "Automated Acceptance Testing with Nightwatch.js",
     date: "2017-03-20",
@@ -222,6 +240,7 @@ In addition to myself, the panel included:
     }],
     slug: "automated-acceptance-testing",
     abstract: "A lightning talk introducing automated acceptance testing concepts, and demonstrating how to write one using Nightwatch.js.",
+    iconUrl: "/assets/nightwatch.jpg",
 },{
     title: "Agile: More Than Just Standups",
     date: "2016-08-04",
@@ -233,6 +252,7 @@ In addition to myself, the panel included:
     }],
     slug: "agile-more-than-just-standups",
     abstract: "A conference talk on what agile is, what it isn't, and how we can embrace it.",
+    iconUrl: "/assets/agile.jpeg",
 },{
     title: "Learning and Teaching JavaScript",
     date: "2015-09-22",
@@ -249,6 +269,7 @@ In addition to myself, the panel included:
     abstract: `
 A presentation on fact vs. folly in how people learn JavaScript (or anything else for that matter).
     `,
+    iconUrl: "/assets/book.png",
 },{
     title: "Ember Vocabulary",
     date: "2015-08-27",
@@ -261,6 +282,7 @@ A presentation on fact vs. folly in how people learn JavaScript (or anything els
     abstract: `
 Based on a suggestion by <a href="https://twitter.com/pixelhandler">@pixelhandler</a>, this was an overview of terminology used in Ember explained using the simplest possible terms. I eventually turned this into the website <a href="http://www.embervocab.com/">embervocab.com</a>.
     `,
+    iconUrl: "/assets/ember.png",
 },{
     title: "How Web Apps Work",
     date: "2015-08-17",
@@ -284,7 +306,8 @@ Based on a suggestion by <a href="https://twitter.com/pixelhandler">@pixelhandle
 A very high-level overview of how web apps work covering front-end, back-end, databases, apps vs. websites, and more.
 
 Photos courtesy of <a href="https://www.flickr.com/photos/adammeek/">Adam Meek</a>.
-    `
+    `,
+    iconUrl: "/assets/web.jpg",
 }, {
     title: "Corner-Cases In Data Modeling",
     slug: "corner-cases-in-data-modeling",
@@ -310,6 +333,7 @@ A presentation on data modeling in Ember.js covering:
         altText: "Kyle presenting",
         url: "/assets/data-modeling-talk-2.jpg",
     }],
+    iconUrl: "/assets/erd.png",
 }, {
     slug: "10-reasons-to-use-ember-in-10-minutes",
     title: "10 Reasons To Use Ember in 10 Minutes",
@@ -320,4 +344,5 @@ A presentation on data modeling in Ember.js covering:
         label: "Slides",
         url: "http://slides.com/kylecoberly/10-reasons-to-use-ember-in-10-minutes",
     }],
+    iconUrl: "/assets/ember.png",
 }]
