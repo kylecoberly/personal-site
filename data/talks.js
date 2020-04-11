27bb8a2d85f94fb63b8c2a383a105edd6b487f2b
Kyle Coberly <kyle.coberly@gmail.com>
2020-04-11 14:58:20 -0600

    Updating styles

--- data/talks.js
+++ data/talks.js
@@ -53,7 +53,7 @@ This talk will be in "Lean Coffee" format, meaning that the audience will contro
         altText: "Topic ghettos",
         url: "/assets/johnny/topic-ghettos.png",
     }],
-    iconUrl: "/assets/book.png",
+    iconUrl: "/assets/icons/apple.svg",
 }, {
     title: "Intro to BDD Syntax",
     date: "2019-09-24",
@@ -69,7 +69,7 @@ This talk will be in "Lean Coffee" format, meaning that the audience will contro
         url: "/assets/intro-to-bdd-talk-1.jpeg",
     }],
     abstract: "Testing doesn't just increase our confidence with code; it also gives us a powerful tool for documenting and communicating expectations with stakeholders. In this talk, Kyle will introduce you to a business-friendly testing syntax that bridges the gap between describing features and confirming their behavior.",
-    iconUrl: "/assets/agile.jpeg",
+    iconUrl: "/assets/icons/computer.svg",
 }, {
     title: "Mind The Gap: Transitioning to Tech Leadership",
     date: "2019-08-14",
@@ -77,7 +77,7 @@ This talk will be in "Lean Coffee" format, meaning that the audience will contro
     venue: "Develop Denver",
     videoUrl: "https://www.youtube.com/embed/hij_gJk55Z0",
     abstract: "Leading people is an entirely different skillset from writing code. You can’t `[^ + c]` when people fail. Fortunately, leadership is a skill you can learn as long as you’re willing to risk vulnerability. In this panel you’ll hear stories and learn methods tech leaders have used as they’ve made the move from individual contributors to people leaders. This will be a collaborative session that uses Poll Everywhere to gather topics and questions from the audience. Bring your questions, challenges and stories, and let the community lend their support.",
-    iconUrl: "/assets/briefcase.png",
+    iconUrl: "/assets/icons/briefcase.svg",
 }, {
     title: "What Should I Test?",
     date: "2019-04-30",
@@ -89,7 +89,7 @@ This talk will be in "Lean Coffee" format, meaning that the audience will contro
         url: "https://slides.com/kylecoberly/what-should-i-test#/",
     }],
     abstract: "Writing tests is easy, but writing meaningful tests is hard. We'll explore some strategies for figuring out what to test and what to leave alone.",
-    iconUrl: "/assets/agile.jpeg",
+    iconUrl: "/assets/icons/computer.svg",
 }, {
     title: "Programming Without Ifs",
     date: "2019-01-17",
@@ -101,7 +101,7 @@ This talk will be in "Lean Coffee" format, meaning that the audience will contro
         url: "https://slides.com/kylecoberly/programming-without-ifs#/",
     }],
     abstract: "The if statement is one of the first of things we learn as programmers. Should it be one of the first things we take out of our vocabulary as well? We'll talk about what the benefits of programmer without ifs are, as well as learn some tricks for getting rid of them.",
-    iconUrl: "/assets/web.jpg",
+    iconUrl: "/assets/icons/computer.svg",
 }, {
     title: "Ember Services & State",
     date: "2015-27-05",
@@ -112,7 +112,7 @@ This talk will be in "Lean Coffee" format, meaning that the audience will contro
         url: "https://slides.com/kylecoberly/deck#/",
     }],
     abstract: "A presentation on how to manage state in an Ember application.",
-    iconUrl: "/assets/ember.png",
+    iconUrl: "/assets/icons/computer.svg",
 }, {
     title: "Suck Less At Networking",
     date: "2018-10-18",
@@ -123,7 +123,7 @@ This talk will be in "Lean Coffee" format, meaning that the audience will contro
         url: "https://github.com/developdenver/suck-less-at-networking",
     }],
     abstract: "A workshop from Develop Denver 2018 on improving your networking skills.",
-    iconUrl: "/assets/briefcase.png",
+    iconUrl: "/assets/icons/briefcase.svg",
 }, {
     title: "Suck Less At Interviewing",
     date: "2018-10-18",
@@ -134,7 +134,7 @@ This talk will be in "Lean Coffee" format, meaning that the audience will contro
         url: "https://github.com/developdenver/suck-less-at-interviewing",
     }],
     abstract: "A workshop from Develop Denver 2018 on improving your interviewing skills.",
-    iconUrl: "/assets/briefcase.png",
+    iconUrl: "/assets/icons/briefcase.svg",
 }, {
     title: "Application Layout With Ember",
     date: "2016-03-16",
@@ -145,7 +145,7 @@ This talk will be in "Lean Coffee" format, meaning that the audience will contro
         url: "https://slides.com/kylecoberly/deck-6-21#/",
     }],
     abstract: "A presentation on how to structure an Ember app to match your visual hierarchy.",
-    iconUrl: "/assets/ember.png",
+    iconUrl: "/assets/icons/computer.svg",
 }, {
     title: "Customizing Ember Data",
     date: "2015-09-25",
@@ -156,7 +156,7 @@ This talk will be in "Lean Coffee" format, meaning that the audience will contro
         url: "https://slides.com/kylecoberly/customizing-ember-data#/",
     }],
     abstract: "A presentation on how to override Ember Data's powerful defaults to meet your application's needs.",
-    iconUrl: "/assets/ember.png",
+    iconUrl: "/assets/icons/computer.svg",
 },{
     title: "Inheritance & Composition With Vue Components",
     date: "2018-11-25",
@@ -167,7 +167,7 @@ This talk will be in "Lean Coffee" format, meaning that the audience will contro
         url: "https://slides.com/kylecoberly/inheritance-and-composition-with-vue-components#/",
     }],
     abstract: "A lightning talk on Vue inheritance and Vue mixins.",
-    iconUrl: "/assets/vue.png",
+    iconUrl: "/assets/icons/computer.svg",
 }, {
     title: "App Architecture with Vue",
     date: "2018-07-22",
@@ -184,7 +184,7 @@ This talk will be in "Lean Coffee" format, meaning that the audience will contro
         url: "https://docs.google.com/document/d/1xRdwHND1fKuEU8HNYEdWpMN9V4EtV11oErLGgZPvJhs/edit?usp=sharing",
     }],
     abstract: "A presentation on the model-view-component architecture pattern in Vue.",
-    iconUrl: "/assets/vue.png",
+    iconUrl: "/assets/icons/computer.svg",
 }, {
     title: "Front-End/Face-Off - Round 2",
     date: "2018-09-20",
@@ -192,7 +192,7 @@ This talk will be in "Lean Coffee" format, meaning that the audience will contro
     venue: "Denver Node",
     slug: "front-end-face-off-round-2",
     abstract: "Brooks, CJ, and I returned for round 2 of the Front-End/Face-Off. This time, I represented Team Vue.",
-    iconUrl: "/assets/vue.png",
+    iconUrl: "/assets/icons/computer.svg",
 }, {
     title: "Unlearning Business",
     date: "2018-07-17",
@@ -200,7 +200,7 @@ This talk will be in "Lean Coffee" format, meaning that the audience will contro
     venue: "Denver Leadership Meetup",
     slug: "unlearning-business",
     abstract: "I spoke at the Denver Leadership meetup about how and why we should stop \"playing business\" and turn off our autopilot with some common business ideas.",
-    iconUrl: "/assets/briefcase.png",
+    iconUrl: "/assets/icons/briefcase.svg",
 }, {
     title: "AMA - Full-Stack Engineer",
     date: "2018-04-10",
@@ -208,7 +208,7 @@ This talk will be in "Lean Coffee" format, meaning that the audience will contro
     venue: "Video Conference",
     slug: "ama-full-stack-engineer",
     abstract: "I spoke with a [group of recruiters](https://www.eventbrite.com/e/ask-me-anything-fullstack-engineer-tickets-44247970872) about full-stack engineering is, what I think distinguishes full-stackers from specialists, and what I look for in a job opportunity.",
-    iconUrl: "/assets/web.jpg",
+    iconUrl: "/assets/icons/computer.svg",
 },{
     title: "Double-Loop TDD",
     date: "2018-03-27",
@@ -223,14 +223,14 @@ This talk will be in "Lean Coffee" format, meaning that the audience will contro
     }],
     slug: "double-loop-tdd",
     abstract: "I spoke about the practice of double-loop TDD, which combines high and low level tests to provide tight feedback loops that also keep you focused on the big picture.",
-    iconUrl: "/assets/agile.jpeg",
+    iconUrl: "/assets/icons/computer.svg",
 }, {
     title: "Web Application Architecture Guest Lecture",
     date: "2017-11-16",
     venue: "Metropolitan State University of Denver",
     slug: "metro-web-application-architecture",
     abstract: "I spoke to a class of about 30 Computer Science students about the MVC architecture pattern, how that's probably not the metaphor we would use if we were starting over today, and the state of modern web development.",
-    iconUrl: "/assets/web.jpg",
+    iconUrl: "/assets/icons/computer.svg",
 },{
     title: "Everything You've Ever Wanted To Know From a Technologist But Were Too Shy To Ask",
     date: "2017-09-25",
@@ -254,7 +254,7 @@ In addition to myself, the panel included:
 * Ashly Carter, UX Designer at Deloitte Digital
 * Kirk Moore, Data Strategist, formerly SVP of Consumer Experience at Craftsy
     `,
-    iconUrl: "/assets/web.jpg",
+    iconUrl: "/assets/icons/computer.svg",
 }, {
     title: "The 5 Developers I Never Want To Work With Again",
     date: "2017-08-11",
@@ -270,7 +270,7 @@ In addition to myself, the panel included:
     }],
     slug: "the-5-developers-i-never-want-to-work-with-again",
     abstract: "A sardonic look at how we act as developers, how we think of developers, and why we need to change both them.",
-    iconUrl: "/assets/briefcase.png",
+    iconUrl: "/assets/icons/briefcase.svg",
 }, {
     title: "Isn't Ember Dead?",
     date: "2017-07-27",
@@ -292,7 +292,7 @@ In addition to myself, the panel included:
     }],
     slug: "isnt-ember-dead",
     abstract: "The Front-End/Face-Off was put on by the Denver Node.js meetup. It was a common set of stories that were live-coded in front of an audience in Ember, Angular, React, and Vanilla. I coded the app in Ember, and give some insight into the thought process of an Ember developer.",
-    iconUrl: "/assets/ember.png",
+    iconUrl: "/assets/icons/computer.svg",
 }, {
     title: "Ember Test Selectors and Page Objects",
     date: "2017-05-31",
@@ -303,7 +303,7 @@ In addition to myself, the panel included:
     }],
     slug: "ember-test-selectors-and-page-objects",
     abstract: "A lightning talk about how to use Ember Test Selectors and Ember Page Objects to add healthy abstractions and flexibility in your Ember tests.",
-    iconUrl: "/assets/ember.png",
+    iconUrl: "/assets/icons/computer.svg",
 },{
     title: "Approaches To Innovation",
     date: "2017-05-25",
@@ -314,7 +314,7 @@ In addition to myself, the panel included:
     }],
     slug: "approaches-to-innovation",
     abstract: "A guest lecture on innovation for DU's masters program in Strategic Innovation and Change.",
-    iconUrl: "/assets/briefcase.png",
+    iconUrl: "/assets/icons/briefcase.svg",
 },{
     title: "Progressive Web Apps Are The Future",
     date: "2017-05-17",
@@ -333,7 +333,7 @@ In addition to myself, the panel included:
     }],
     slug: "progressive-web-apps-are-the-future",
     abstract: "A talk for the TechCrunch Disrupt NYC conference where I talk about the history of the web, and how some of the new browser technologies take on the advantages of native app with few of their drawbacks.",
-    iconUrl: "/assets/pwa.jpg",
+    iconUrl: "/assets/icons/computer.svg",
 }, {
     title: "Automated Acceptance Testing with Nightwatch.js",
     date: "2017-03-20",
@@ -344,7 +344,7 @@ In addition to myself, the panel included:
     }],
     slug: "automated-acceptance-testing",
     abstract: "A lightning talk introducing automated acceptance testing concepts, and demonstrating how to write one using Nightwatch.js.",
-    iconUrl: "/assets/nightwatch.jpg",
+    iconUrl: "/assets/icons/computer.svg",
 },{
     title: "Agile: More Than Just Standups",
     date: "2016-08-04",
@@ -356,7 +356,7 @@ In addition to myself, the panel included:
     }],
     slug: "agile-more-than-just-standups",
     abstract: "A conference talk on what agile is, what it isn't, and how we can embrace it.",
-    iconUrl: "/assets/agile.jpeg",
+    iconUrl: "/assets/icons/briefcase.svg",
 },{
     title: "Learning and Teaching JavaScript",
     date: "2015-09-22",
@@ -373,7 +373,7 @@ In addition to myself, the panel included:
     abstract: `
 A presentation on fact vs. folly in how people learn JavaScript (or anything else for that matter).
     `,
-    iconUrl: "/assets/book.png",
+    iconUrl: "/assets/icons/apple.svg",
 },{
     title: "Ember Vocabulary",
     date: "2015-08-27",
@@ -386,7 +386,7 @@ A presentation on fact vs. folly in how people learn JavaScript (or anything els
     abstract: `
 Based on a suggestion by <a href="https://twitter.com/pixelhandler">@pixelhandler</a>, this was an overview of terminology used in Ember explained using the simplest possible terms. I eventually turned this into the website <a href="http://www.embervocab.com/">embervocab.com</a>.
     `,
-    iconUrl: "/assets/ember.png",
+    iconUrl: "/assets/icons/computer.svg",
 },{
     title: "How Web Apps Work",
     date: "2015-08-17",
@@ -411,7 +411,7 @@ A very high-level overview of how web apps work covering front-end, back-end, da
 
 Photos courtesy of <a href="https://www.flickr.com/photos/adammeek/">Adam Meek</a>.
     `,
-    iconUrl: "/assets/web.jpg",
+    iconUrl: "/assets/icons/computer.svg",
 }, {
     title: "Corner-Cases In Data Modeling",
     slug: "corner-cases-in-data-modeling",
@@ -437,7 +437,7 @@ A presentation on data modeling in Ember.js covering:
         altText: "Kyle presenting",
         url: "/assets/data-modeling-talk-2.jpg",
     }],
-    iconUrl: "/assets/erd.png",
+    iconUrl: "/assets/icons/computer.svg",
 }, {
     slug: "10-reasons-to-use-ember-in-10-minutes",
     title: "10 Reasons To Use Ember in 10 Minutes",
@@ -448,5 +448,5 @@ A presentation on data modeling in Ember.js covering:
         label: "Slides",
         url: "http://slides.com/kylecoberly/10-reasons-to-use-ember-in-10-minutes",
     }],
-    iconUrl: "/assets/ember.png",
+    iconUrl: "/assets/icons/computer.svg",
 }]
